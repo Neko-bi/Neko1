896d83efbc259127d20182e4cc639dde3a6557d2
Neko-bi <l7g6h4@gmail.com>
2025-07-04 16:43:06 -0400

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -35,3 +35,6 @@ switch (userType) {
         userCategory = "Unknown";
 }
 console.log("User Category:", userCategory);
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
