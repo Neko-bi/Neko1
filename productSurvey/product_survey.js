773bb83b5e78c6c1241367b920ff664eaa33cc0b
Neko-bi <l7g6h4@gmail.com>
2025-07-10 13:50:29 -0400

    message

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,7 +6,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-
+    const experience = document.getElementById('userExperience').value;
     
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -15,6 +15,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML - experience;
     document.getElementById('userInfo').style.display = 'block';
     alert('Thank you for your valuable feedback')
 }
