96f1ee034c778e3b3564e5a212ab62ea100051c9
Neko-bi <l7g6h4@gmail.com>
2025-07-11 11:17:04 -0400

    message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,12 @@
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+breakfastMenu.unshift("Pancakes- $12", "Eggs Benedict -$22.99", "Oatmeal -$21.99", "Frittata -$15");
+breakfastMenu.length = 4 
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+mainCourseMenu.unshift("Steak- $22", "Pasta -$12.99", "Burger -$21.99", "Salmomn -$18");
+mainCourseMenu.length = 4 
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+dessertMenu.unshift("Cake - $3", "Ice cream -$4", "Pudding -$6", "Fruit Salad -$4");
+dessertMenu.length = 4 
 
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
@@ -14,3 +20,5 @@ let dessertItem = '';
 for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
+console.log(breakfastMenu.length);
